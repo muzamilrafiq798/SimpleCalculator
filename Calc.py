5329cabfa512e236b2bc14165459baa9cd247916
muzamilrafiq798 <55424486+muzamilrafiq798@users.noreply.github.com>
2025-11-10 12:23:11 +0500

    new features added
    You can now press 'q' to exit the calculator.
    Otherwise, it will run in a loop.

--- Calc.py
+++ Calc.py
@@ -1,40 +1,42 @@
-from operator import truediv
 
-a = float(input("Enter first number: "))
-b = float(input("Enter second number: "))
-op = input("Enter operation(+, -, *, /): ")
+def sum(a,b): return a+b
 
-def sum(a,b):
-    return a+b
+def sub(a, b): return a-b
 
-def sub(a, b):
-    return a-b
+def mul(a, b): return a*b
 
-def mul(a, b):
-    return a*b
+def div(a, b): return a/b if b!=0 else "Cannot divide by zero."
 
-def div(a, b):
-    return a/b if b!=0 else "Can't divide by zero."
-
-def get_number(a, b):
-    while(True):
+def get_number(prompt):
+    while True:
         try:
             return float(input(prompt))
         except ValueError:
             print("Invalid input")
 
-if op == "+":
-    print(sum(a, b))
 
-elif op == "-":
-    print(sub(a, b))
+while True:
+    print("\n____SimpleCalculator____")
+    a = get_number("Enter first number: ")
+    b = get_number("Enter second number: ")
+    op = input("Enter operation(+, -, *, /): ")
 
-elif op == "*":
-    print(mul(a, b))
+    if op.lower() == "q":
+        print("Goodbye!")
+        break
 
-elif op == "/":
-    print(div(a, b))
 
-else:
-    print("Invalid operation")
+    if op == "+":
+        print("Result: ", sum(a, b))
 
+    elif op == "-":
+        print("Result: ", sub(a, b))
+
+    elif op == "*":
+        print("Result: ", mul(a, b))
+
+    elif op == "/":
+        print("Result: ", div(a, b))
+
+    else:
+        print("Invalid operation")
